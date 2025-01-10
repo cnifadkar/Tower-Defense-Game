a647f5093ab507c6b4c85a17c23908c6386cc1c4
Chaitanya Nifadkar <chaitanya.nifadkar@gmail.com>
2025-01-10 15:07:32 -0500

    Delete Tower-Defense-main directory

--- Tower-Defense-main/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.mainscreen {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires junit;
-
-
-    opens com.example.mainscreen to javafx.fxml;
-    exports com.example.mainscreen;
-}
